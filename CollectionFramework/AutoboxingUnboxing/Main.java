5675467d655c7f1452ade32370275990c7c13452
Rajat kumar <rohit.r.sharma2@gmail.com>
2024-09-24 14:03:47 +0530

    autoboxing

---
+++ CollectionFramework/AutoboxingUnboxing/Main.java
@@ -0,0 +1,12 @@
+package CollectionFramework.AutoboxingUnboxing;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> nums = new ArrayList<>();
+        nums.add(1);
+        nums.add(2);
+        nums.add(3);
+        System.out.println(nums);
+    }
+}
