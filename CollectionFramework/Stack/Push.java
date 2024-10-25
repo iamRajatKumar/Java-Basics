44e1509578bf467953a5a74e8f70c37435c20bf9
iamRajatKumar <rohit.r.sharma2@gmail.com>
2024-10-25 23:57:59 +0530

    push method

---
+++ CollectionFramework/Stack/Push.java
@@ -0,0 +1,17 @@
+package CollectionFramework.Stack;
+import java.util.Stack;
+
+public class Push {
+
+    public static void main(String[] args) {
+        Stack<Integer> nums = new Stack<>();
+        nums.push(25);
+        nums.push(20);
+        nums.push(23);
+        nums.push(28);
+        nums.push(29);
+        nums.push(21);
+        System.out.println(nums);
+    }
+    
+}
