076c6f8d74ef3ccf29a58255ab80d0411832bb86
iamRajatKumar <rohit.r.sharma2@gmail.com>
2024-10-25 23:55:22 +0530

    stack

---
+++ CollectionFramework/Stack/Stack.java
@@ -0,0 +1,11 @@
+package CollectionFramework.Stack;
+import java.util.*;
+
+public class Stack {
+
+    public static void main(String[] args) {
+        Stack<Integer> numbers= new Stack<>();
+        numbers.push
+    }
+    
+}
