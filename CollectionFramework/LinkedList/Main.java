eca3798f084d014b94a1da8b1486a75d586babbd
iamRajatKumar <rohit.r.sharma2@gmail.com>
2024-10-27 01:47:10 +0530

    commit

--- CollectionFramework/LinkedList/Main.java
+++ CollectionFramework/LinkedList/Main.java
@@ -1,7 +1,22 @@
 package CollectionFramework.LinkedList;
+import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        
-    }
+        LinkedList<String> names = new LinkedList<>();
+        names.add("Rajat");
+        names.add("Deepali");
+        names.add("Priya");
+        names.add("varun");
+        names.add("Sonal");
+        names.add("Deepu");
+        //   
+        System.out.println(names);
+        names.add("jhon");
+        // after adding a new name in the end
+        System.out.println(names);
+        // adding a name after a given index in linked list
+        names.add(2, "Rajdeep");
+        System.out.println(names);
+        }
 }
