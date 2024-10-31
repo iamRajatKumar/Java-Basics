a8eb67f2618d0601a91c5d40a803c0083a375331
iamRajatKumar <rohit.r.sharma2@gmail.com>
2024-10-31 23:09:50 +0530

    L5_BoundedTypeParams custom wrapper NAME added

--- Generics/L5_BoundedTypeParams.java
+++ Generics/L5_BoundedTypeParams.java
@@ -1,13 +1,13 @@
 package Generics;
 
-public class L5_BoundedTypeParams {
-   public static void main(String[] args) {
-    Datum<Integer, String> data = new Datum<>(1, "Deepali");
+public class L5_BoundedTypeParams { b    public static void main(String[] args) {
+    Datum<Integer, Name> data = new Datum<>(1, new Name("Deepali"));
+    System.out.println(data);
     data.display(27.5432, "Rajat");
    } 
 }
 
-class Datum<K,V>{
+class Datum<K extends Integer,V extends Name>{
     
     private K key;
     private V value;
@@ -28,6 +28,25 @@ class Datum<K,V>{
     // here we can't use any other data type instead of Number
     public <N extends Number,E > void display(N number, E element){
         System.out.println("Number : "+number+" Element : " +element);
+    }
+
+    @Override
+    public String toString() {
+        return "Datum [key=" + key + ", value=" + value + "]";
+    }
+    
+}
+
+class Name{
+    private String name;
+
+    public Name(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Name [name=" + name + "]";
     }
     
 }
