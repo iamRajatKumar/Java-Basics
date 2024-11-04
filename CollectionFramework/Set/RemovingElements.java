ac34b9058c420e40a3d04a9e66e4357bd3ede3e6
iamRajatKumar <rohit.r.sharma2@gmail.com>
2024-11-04 22:02:08 +0530

    Remove from set

--- CollectionFramework/Set/RemovingElements.java
+++ CollectionFramework/Set/RemovingElements.java
@@ -1,5 +1,21 @@
 package CollectionFramework.Set;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class RemovingElements {
+   public static void main(String[] args) {
     
+    Set<String> set =  new HashSet<String>();
+    set.add("Rajat");
+    set.add("Sharma");
+    set.add("Deepali");
+    set.add("Ravi");
+    set.add("Gargi");
+
+    System.out.println("Initial value of Set"+set);
+
+    set.remove("Sharma");
+    System.out.println("Set value after removing"+set);
+   } 
 }
