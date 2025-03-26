6352f3e1edfc28c69c32a544df19df8d7ae3b1de
iamRajatKumar <rohit.r.sharma2@gmail.com>
2025-03-26 21:59:08 +0530

    Multithreading first commit

---
+++ MultiThreading/Lecture1.java
@@ -0,0 +1,31 @@
+package MultiThreading;
+
+import static java.lang.Thread.sleep;
+
+class MyCounter{
+    private int threadNo;
+    
+    public MyCounter(int threadNo) {
+        this.threadNo = threadNo;
+    }
+    public void countMe() throws InterruptedException{
+        for(int i =0; i<10; i++){
+            sleep(500);
+            System.out.println("Thread no: "+ threadNo +" and iteration no: "+i);
+        }
+    }
+
+}
+
+public class Lecture1 {
+    public static void main(String[] args) throws InterruptedException{
+      MyCounter counter1 = new MyCounter(1);
+      MyCounter counter2 = new MyCounter(2);
+      long startTime = System.currentTimeMillis();
+      counter1.countMe();
+      System.out.println("--------------------------------------------");
+        counter2.countMe();
+        long endTime = System.currentTimeMillis();
+        System.out.println("Total time for processing : "+(endTime -startTime));
+    }
+}
