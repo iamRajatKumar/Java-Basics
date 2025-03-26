7c13ba2f5485133d4e7f465a794349ff99989d7d
iamRajatKumar <rohit.r.sharma2@gmail.com>
2025-03-27 00:00:59 +0530

    Runnable Interface Continues

---
+++ MultiThreading/L2_RunnableInterface/RunnableInterfaceContinues.java
@@ -0,0 +1,26 @@
+package MultiThreading.L2_RunnableInterface;
+
+import java.util.Random;
+import static java.lang.Thread.sleep;
+
+public class RunnableInterfaceContinues {
+    public static void main(String[] args) throws InterruptedException{
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                for(int i=0; i<10; i++){
+                    try {
+                        Random random = new Random();
+                        sleep(random.nextInt(1000));
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(i);
+                }
+            }
+            
+        }).start();
+        
+    }
+}
