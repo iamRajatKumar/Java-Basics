87347e518b9fc8bd39e447de500415e23ecd5d9b
iamRajatKumar <rohit.r.sharma2@gmail.com>
2025-03-26 23:21:36 +0530

    lecture2

---
+++ MultiThreading/Lecture2.java
@@ -0,0 +1,45 @@
+package MultiThreading;
+
+import static java.lang.Thread.sleep;
+
+class MyCounter extends Thread{
+    private int threadNo;
+    
+    public MyCounter(int threadNo) {
+        this.threadNo = threadNo;
+    }
+
+    public void countMe() throws InterruptedException{
+        for(int i =0; i<10; i++){
+            sleep(500);
+            System.out.println("Thread no: "+ threadNo +" and iteration no: "+i);
+        }
+    }
+
+    @Override
+    public void run() {
+        try{
+            countMe();
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
+    
+    }
+
+}
+
+public class Lecture2 {
+    public static void main(String[] args) throws InterruptedException{
+        MyCounter counter1 = new MyCounter(1);
+        MyCounter counter2 = new MyCounter(2);
+        MyCounter counter3 = new MyCounter(3);
+        long startTime = System.currentTimeMillis();
+        counter1.start();
+        System.out.println("--------------------------------------------");
+        counter2.start();
+        counter3.start();
+        sleep(5100);
+        long endTime = System.currentTimeMillis();
+        System.out.println("Total time for processing : "+(endTime -startTime));
+    }
+}
