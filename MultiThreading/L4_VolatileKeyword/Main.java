e4e119a1b5189c196e2642c627882f2a74a02446
iamRajatKumar <rohit.r.sharma2@gmail.com>
2025-03-28 22:51:35 +0530

    Volatile keyword

---
+++ MultiThreading/L4_VolatileKeyword/Main.java
@@ -0,0 +1,34 @@
+package MultiThreading.L4_VolatileKeyword;
+
+public class Main {
+    volatile public static int flag = 0;
+    public static void main(String[] args) {
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                int i = 0;
+                while(true){
+                    if(flag == 0){
+                        System.out.println(i+" : Running...");
+                    }
+                }
+            }
+            
+        }).start();
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                flag = 1;
+                System.out.println("The Value of Flag updated, and thread 1 stopped");
+            }
+        
+        }).start();
+    }
+}
