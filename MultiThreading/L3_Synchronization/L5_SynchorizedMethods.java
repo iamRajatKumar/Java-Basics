8b4ab8ad097e85f14a77e30843e66ace55c10ee1
Rajat kumar <rohit.r.sharma2@gmail.com>
2025-03-28 22:49:41 +0530

    Add files via upload

---
+++ MultiThreading/L3_Synchronization/L5_SynchorizedMethods.java
@@ -0,0 +1,41 @@
+package Multithreading;
+
+class Brackets{
+    //synchronized is used in threads to generate synchronized result
+    synchronized public void generate(){
+        for(int i=0; i<=20;i++){
+            if(i<=10){
+                System.out.print('[');
+            }else{
+                System.out.print(']');
+            }
+        }
+        System.out.println();
+    }
+}
+
+public class L5_SynchorizedMethods {
+    public static void main(String[] args) {
+        Brackets brackets = new Brackets();
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                for(int i=0; i<5; i++){
+                    brackets.generate();
+                }
+            }
+            
+        }).start();
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                for(int i=0; i<5; i++){
+                    brackets.generate();
+                }
+            }
+            
+        }).start();
+    }
+}
