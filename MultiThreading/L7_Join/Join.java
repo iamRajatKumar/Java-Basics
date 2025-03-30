ec829ef04faabeb9357c599d994520f384070308
iamRajatKumar <rohit.r.sharma2@gmail.com>
2025-03-30 20:10:14 +0530

    Join in multithreading

---
+++ MultiThreading/L7_Join/Join.java
@@ -0,0 +1,20 @@
+package MultiThreading.L7_Join;
+
+public class Join {
+    public static int counter1 = 0;
+    public static void main(String[] args)throws InterruptedException {
+        Thread thread1 = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                for(int i = 0; i<1000; i++){
+                    counter1++;
+                }
+            }
+            
+        });
+        thread1.start();
+        thread1.join();
+        System.out.println("Counter1 : "+counter1);
+    }
+}
