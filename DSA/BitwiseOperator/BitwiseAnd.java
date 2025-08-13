a7518e2f8dcceb3338fa30bafd84d406a6891c07
iamRajatKumar <rohit.r.sharma2@gmail.com>
2025-08-13 23:28:47 +0530

    Bitwise and

---
+++ DSA/BitwiseOperator/BitwiseAnd.java
@@ -0,0 +1,8 @@
+public class BitwiseAnd {
+    public static void main(String args[]){
+        int x = 3;
+        int y = 6;
+        System.out.print("value of x & y : ");
+        System.out.print(x & y);
+    }
+}
